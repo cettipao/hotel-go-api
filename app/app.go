2e581a0269aeef9731661f3ba37e4a5b8d38bba3
Paolo Cetti <49447885+cettipao@users.noreply.github.com>
2023-05-10 16:35:47 -0300

    Update app.go

--- app/app.go
+++ app/app.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-api/controllers"
+	"os"
 )
 
 func StartApp() {
 	router := gin.Default()
 	router.GET("/items/:itemID", controllers.GetItem)
-	err := router.Run(":8080")
+	err := router.Run(os.Getenv("PORT"))
 	if err != nil {
 		fmt.Println("Error running app", err)
 	}
